81602c57fe8a0341619da91c2c3202590057228d
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2024-01-14 14:16:55 +0800

    Add files via upload

---
+++ stringinput.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string A,B,C,D,E;
+    cin>> A>>B;
+    cin>>C>>D>>E;
+    cout<<A<<" "<<B<<" "<<C<<" "<<D<<" "<<E;
+
+
+    return 0;
+}
