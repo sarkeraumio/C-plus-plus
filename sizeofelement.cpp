1162934759717c08e5a667c17b92f425fb3696d2
Aumio Kumar Sarker <87389689+sarkeraumio@users.noreply.github.com>
2024-01-15 11:34:07 +0800

    Add files via upload

---
+++ sizeofelement.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+///find the size of the array first
+///then find the number of elelments in the array
+///size of integer is 4
+int  main(){
+    int Bus[5]={10,20,30,40,50};
+    cout<<sizeof(Bus)<<" is the size of all the elements"<<endl;
+    cout<<sizeof(Bus)/4;
+
+    return 0;
+
+}
+
+
+
+
